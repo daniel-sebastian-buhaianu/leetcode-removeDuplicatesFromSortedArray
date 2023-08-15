1f62db221661298c783d05901153290bd549f913
Dan Web <128343993+therealdanweb@users.noreply.github.com>
2023-08-15 19:33:49 +0300

    Update solution.c

--- solution.c
+++ solution.c
@@ -1,1 +1,18 @@
-// TODO
+int removeDuplicates(int* nums, int numsSize){
+    int index = 0;
+
+    int i = 0;
+    while (i < numsSize - 1) {
+        if (nums[i] != nums[i + 1]) {
+            nums[index] = nums[i];
+
+            index++;
+        }
+        
+        i++;
+    }
+
+    nums[index] = nums[i];
+
+    return index + 1;
+}
